3cfa4cbb08f1b20ab33b8ad5a19067cdb0acd8d0
zhangwuba <zhangwuba@droi.com>
2019-04-13 10:08:18 +0800

    fixed mac address for 8111

--- auto-setup.py
+++ auto-setup.py
@@ -25,7 +25,6 @@ def genIP(data):
     return [i for i in lines if i and not i.startswith('lo')]
 
 def parseIP(data):
-    dic = {}
     ethArry = []
     for devs in data:
         lines = devs.split('\n')
@@ -33,18 +32,31 @@ def parseIP(data):
         if 'RUNNING' in lines[0]:
             if 'inet 192.168.100' in lines[1]:
                 devname = lines[0].split()[0]
-                macaddr = lines[0].split()[-1]
+                #macaddr = lines[3].split()[1]
                 ethArry.append(devname.strip(':'))
     return ethArry
+
+def getMac(data):
+    mac = []
+    macaddr = ''
+    for devs in data:
+        lines = devs.split('\n')
+        for line in lines:
+            if 'ether' in line:
+                macaddr = line.split()[1]
+        if 'RUNNING' in lines[0]:
+            if 'inet 192.168.100' in lines[1]:
+                mac.append(macaddr)
+    return mac
 
 def getPxkStatus():
     url = "http://192.168.100.20:5050/pxk/api/v1.0/online"
     r = requests.get(url)
     print r.json()
 
-def setup_dhcp(ip):
+def setup_dhcp(ip,mac):
     url = "http://192.168.100.20:5050/pxk/setup_dhcp"
-    data = {'id':'1','ip':str(ip)}
+    data = {'id':'1','ip':str(ip), 'mac':mac}
     r = requests.post(url, data)
     print r.text
 
@@ -53,6 +65,8 @@ if __name__ == '__main__':
     nics = genIP(data)
     ethArry = parseIP(nics)
     print ethArry
+    mac = getMac(nics)
+    print mac
     #getPxkStatus()
     #setup_dhcp()
     #step 1: down all eth*
@@ -60,20 +74,23 @@ if __name__ == '__main__':
         os.environ['eth'] = eth
         os.system('ifconfig $eth down')
 
-    #step 1, up one by one, and post to nxp
+    #step 1, up on by on, and post to nxp
     ip = 100
     ethmap = {}
+    i = 0;
     for eth in ethArry:
         print eth
         print ip
+        print mac[i]
         os.environ['eth'] = eth
         os.system('ifconfig $eth up')
         os.system('ifconfig $eth 192.168.100.200')
         sleep(10)
-        setup_dhcp(ip)
+        setup_dhcp(ip, mac[i])
         os.system('ifconfig $eth down')
         ethmap[eth] = ip
         ip = ip + 1
+        i = i + 1
 
     for ethname in ethmap:
         print("ifconfig up " + ethname + str(ethmap[ethname]))
